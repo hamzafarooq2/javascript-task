6d5281e901dd7ea78974a79f78b0fe9b8318b4ba
Hamza Farooq <hamzaf0000@gmail.com>
2023-04-17 16:15:09 +0500

    modified

--- task.js
+++ task.js
@@ -7,15 +7,17 @@ function calculate_refund(fare, flight_delay) {
   if (flight_delay <= 30) {
     return "No refund for 30 minutes time delay";
   } else if (flight_delay > 30 && flight_delay <= 120) {
-    return "This is your 10% refund: $" + (10 / 100) * fare; // 10% refund, capped at $100
+    return "This is your 10% refund: $" + (10 / 100) * fare; 
   } else if (flight_delay > 120 && flight_delay <= 300) {
-    return "This is your 20% refund: $" + (20 / 100) * fare; // 20% refund, capped at $100
+    return "This is your 20% refund: $" + (20 / 100) * fare; 
   } else if (flight_delay > 300) {
-    return "This is your 50% refund: $" + (50 / 100) * fare; // 50% refund, capped at $100
+    return "This is your 50% refund: $" + (50 / 100) * fare; 
   } else {
     return -1;
   }
 }
 
-const customer_refund = calculate_refund(fare, flight_delay);
+var customer_refund = calculate_refund(fare, flight_delay);
+
+
 console.log(customer_refund);
